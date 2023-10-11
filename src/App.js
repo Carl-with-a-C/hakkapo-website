71b2474b16db7ac2374622fc0fdd7d8fd05e9af2
Carl-with-a-C <carlbclifton@gmail.com>
2023-10-11 12:26:30 +0100

    top section of hero section complete with basic css no responsiveness

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Nav from "./Components/Nav";
+import Hero from "./Components/Hero";
 import "./sass/main.scss";
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Hero />
     </div>
   );
 }
